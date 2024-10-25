f39eb32eaaf9d55b206c33b021003c8d72baaa18
Mihir Bari <barimihir23gmail.com>
2024-10-25 15:40:49 +0530

    Ok

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //const API_BASE_URL = "http://43.204.100.186:8080";
-const API_BASE_URL = "http://localhost:8080";
+//const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://crm-cdjx.onrender.com";
 
 export default API_BASE_URL;
